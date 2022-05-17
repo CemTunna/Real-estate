d7c6b8e67d0bd6393e2bd881feaf353c0aefdcf8
CemTunna <cemroot@gmail.com>
2022-05-17 19:02:57 +0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import A from '@/components/A';
+import React, { Fragment } from 'react';
 import { Theme } from '@/theme/Theme';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Explore from '@/pages/Explore';
+import SignIn from '@/pages/SignIn';
+import SignUp from '@/pages/SignUp';
+import Profile from '@/pages/Profile';
+import Offers from '@/pages/Offers';
+import ForgotPassword from '@/pages/ForgotPassword';
 function App() {
   return (
-    <div className='App'>
-      <A />
-    </div>
+    <Fragment>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<SignIn />} />
+          <Route path='/signIn' element={<SignIn />} />
+          <Route path='/signUp' element={<SignUp />} />
+          <Route path='/forgotPassword' element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </Fragment>
   );
 }
 
