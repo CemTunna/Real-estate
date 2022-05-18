24d6b22af90428c186b59ecfbab541898c9fa5f7
CemTunna <cemroot@gmail.com>
2022-05-18 16:36:48 +0300

    signIn , signUp

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,7 +1,63 @@
-import React from 'react';
-
+import { Button, Grid, IconButton, Typography } from '@mui/material';
+import React, { Fragment, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const { email, password } = formData;
+  const navigate = useNavigate();
+  const onChange = (e: React.ChangeEventHandler<HTMLInputElement>) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+  return (
+    <Fragment>
+      <Grid>
+        <header>
+          <Typography>Welcome back</Typography>
+        </header>
+        <form action=''>
+          <input
+            onChange={onChange}
+            placeholder='Email'
+            id='email'
+            value={email}
+            type='email'
+          />
+          <Grid>
+            <input
+              placeholder='Password'
+              id='password'
+              value={password}
+              onChange={onChange}
+              type={showPassword ? 'text' : 'password'}
+            />
+            <IconButton
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            >
+              <VisibilityIcon />
+            </IconButton>
+          </Grid>
+          <Link to='/forgotPassword'>Forgot Password</Link>
+          <Grid>
+            <Button>
+              Sign In
+              <ArrowForwardIosIcon />
+            </Button>
+          </Grid>
+        </form>
+        {/* google oath */}
+        <Link to='/signUp'>Sign Up</Link>
+      </Grid>
+    </Fragment>
+  );
 };
 
 export default SignIn;
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,7 +1,71 @@
-import React from 'react';
-
+import { Button, Grid, IconButton, Typography } from '@mui/material';
+import React, { Fragment, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    name: '',
+  });
+  const { email, password, name } = formData;
+  const navigate = useNavigate();
+  const onChange = (e: React.ChangeEventHandler<HTMLInputElement>) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+  return (
+    <Fragment>
+      <Grid>
+        <header>
+          <Typography>Welcome back</Typography>
+        </header>
+        <form action=''>
+          <input
+            onChange={onChange}
+            placeholder='Name'
+            id='name'
+            value={name}
+            type='text'
+          />
+          <input
+            onChange={onChange}
+            placeholder='Email'
+            id='email'
+            value={email}
+            type='email'
+          />
+          <Grid>
+            <input
+              placeholder='Password'
+              id='password'
+              value={password}
+              onChange={onChange}
+              type={showPassword ? 'text' : 'password'}
+            />
+            <IconButton
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            >
+              <VisibilityIcon />
+            </IconButton>
+          </Grid>
+          <Link to='/forgotPassword'>Forgot Password</Link>
+          <Grid>
+            <Button>
+              Sign Up
+              <ArrowForwardIosIcon />
+            </Button>
+          </Grid>
+        </form>
+        {/* google oath */}
+        <Link to='/signIn'>Sign In</Link>
+      </Grid>
+    </Fragment>
+  );
 };
 
 export default SignUp;
