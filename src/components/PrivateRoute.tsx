d620f8faef648dc1f1ca65073455f5e13e5ca867
CemTunna <cemroot@gmail.com>
2022-05-23 18:06:42 +0300

    spinner

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,9 +1,23 @@
 import useAuthStatus from '@/hooks/useAuthStatus';
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import { CircularProgress, Box } from '@mui/material';
 const PrivateRoute = () => {
   const { loggedIn, status } = useAuthStatus();
-  if (status) return <h3>Loading...</h3>;
+  if (status) {
+    return (
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+        }}
+      >
+        <CircularProgress color='secondary' size={70} />
+      </Box>
+    );
+  }
   return loggedIn ? <Outlet /> : <Navigate to='/signIn' />;
 };
 
