39d2b548439faec34086e6f991b5db7ee62ef459
CemTunna <cemroot@gmail.com>
2022-05-26 22:25:34 +0300

    last version

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from 'firebase/firestore';
+import { db } from '@/firebase';
+import { toast } from 'react-toastify';
 
 const Category = () => {
+  const params = useParams();
+  useEffect(() => {
+    const getList = async () => {
+      try {
+        const listRef = collection(db, 'listings');
+        const q = query(listRef, where('type', '==', params.categoryType));
+      } catch (error) {}
+    };
+    getList();
+  });
   return <div>Category</div>;
 };
 
