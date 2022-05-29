c6e023df71cec3ad60896a09d928d21884adff18
CemTunna <cemroot@gmail.com>
2022-05-29 21:21:25 +0300

    offer

--- src/pages/Offers.tsx
+++ src/pages/Offers.tsx
@@ -1,7 +1,99 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+  getDoc,
+} from 'firebase/firestore';
+import { db } from '@/firebase';
+import { toast } from 'react-toastify';
+import Subtitle from '@/components/Subtitle';
+import { List } from '@mui/material';
+import ReListItem from '@/components/BReListItem';
+import { Listing } from '@/interfaces/Listing';
+import { makeStyles } from 'tss-react/mui';
+import Container from '@/components/Container';
+import Loader from '@/components/Loader';
+import BReText from '@/components/BReText';
+interface ListingArray {
+  data: Listing;
+  id: string;
+}
+
+const useStyles = makeStyles()((theme) => ({
+  list: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  main: {
+    width: '100%',
+    height: '100%',
+    alignSelf: 'flex-start',
+  },
+  text: {
+    fontWeight: theme.typography.fontWeightBold,
+    margin: '2rem',
+  },
+}));
 
 const Offers = () => {
-  return <div>Offers</div>;
+  const { classes } = useStyles();
+
+  const params = useParams();
+  const [listing, setListing] = useState<ListingArray[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    const getList = async () => {
+      try {
+        const listRef = collection(db, 'listings');
+        const q = query(
+          listRef,
+          where('offer', '==', true),
+          orderBy('timestamp', 'desc'),
+          limit(10)
+        );
+        const querySnap = await getDocs(q);
+        let listings: any = [];
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+
+        setListing(listings);
+        setLoading(false);
+      } catch (error) {
+        toast.error("Couldn't get listings");
+      }
+    };
+    getList();
+  }, []);
+  return (
+    <Container>
+      <header>
+        <Subtitle>Offers</Subtitle>
+      </header>
+      {loading ? (
+        <Loader />
+      ) : listing && listing.length > 0 ? (
+        <main className={classes.main}>
+          <List className={classes.list}>
+            {listing.map((item) => (
+              <ReListItem listing={item.data} id={item.id} key={item.id} />
+            ))}
+          </List>
+        </main>
+      ) : (
+        <BReText className={classes.text}>{`No listing`}</BReText>
+      )}
+    </Container>
+  );
 };
 
 export default Offers;
