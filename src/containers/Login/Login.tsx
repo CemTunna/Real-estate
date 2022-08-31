043d490422f94bff506b41d85e02e415ee5c8be6
Cem Tuna Koç <cemroot@gmail.com>
2022-08-31 19:44:09 +0300

    fix- login refactor

---
+++ src/containers/Login/Login.tsx
@@ -0,0 +1,75 @@
+import { Grid, IconButton, InputAdornment } from '@mui/material';
+import useStyles from './LoginStyles';
+import React, { Fragment, useEffect, useState } from 'react';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import useForm from '@/hooks/useForm';
+import Subtitle from '@/components/Subtitle';
+import BRealButton from '@/components/BRealButton';
+import Form from '@/components/formm/Form';
+import Container from '@/components/Container';
+import OAuth from '@/components/oauth/OAuth';
+import Input from '@/components/ui/input/Input';
+const Login = () => {
+  const { classes } = useStyles();
+
+  const { email, password, onChange, showPassword, setShowPassword, onSubmit } =
+    useForm();
+
+  return (
+    <Fragment>
+      <Container>
+        <header>
+          <Subtitle>Welcome Back</Subtitle>
+        </header>
+        <Form onSubmit={onSubmit}>
+          <Input
+            autoFocus={true}
+            id='email'
+            onChange={onChange}
+            value={email!}
+            placeholder='Email'
+            type='email'
+            className={classes.input}
+          />
+          <Input
+            className={classes.input}
+            placeholder='Password'
+            id='password'
+            value={password!}
+            onChange={onChange}
+            type={showPassword ? 'text' : 'password'}
+            propss={{
+              endAdornment: (
+                <InputAdornment position='end'>
+                  <IconButton
+                    onClick={() => setShowPassword((prevState) => !prevState)}
+                  >
+                    <VisibilityIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          <Link to='/forgotPassword' className={classes.link}>
+            Can't Sign In ?
+          </Link>
+          <Grid style={{ display: 'flex' }}>
+            <BRealButton className={classes.btn} type='submit'>
+              Sign In
+              <ArrowForwardIosIcon />
+            </BRealButton>
+          </Grid>
+        </Form>
+        <OAuth />
+        <Link to='/signUp' className={classes.link}>
+          Sign Up
+        </Link>
+      </Container>
+    </Fragment>
+  );
+};
+
+export default Login;
